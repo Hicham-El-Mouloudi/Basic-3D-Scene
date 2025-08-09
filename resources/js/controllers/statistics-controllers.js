e510d79321027524c72137b7749fbd1764cab2be
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-08-09 20:59:57 +0100

    Implementing the initializeStatsGUI function

--- resources/js/controllers/statistics-controllers.js
+++ resources/js/controllers/statistics-controllers.js
@@ -3,4 +3,14 @@ import Stats from "../three/examples/jsm/libs/stats.module"
 
 export function initializeStatsGUI({mainHTMLElement}){
     // 
+    const stats = Stats();
+    if(mainHTMLElement?? false){
+        mainHTMLElement.appendChild(stats);
+    } else {
+        document.body.appendChild(stats);
+    }
+    return () => {
+        // 
+        stats.update();
+    }
 }

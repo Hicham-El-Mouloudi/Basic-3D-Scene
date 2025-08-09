1ea0bb1241bdac40349a8f668c7e57a88b87e7b4
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-08-09 23:13:49 +0100

    Adding a random size

--- resources/js/functionalities/cubeAdditions.js
+++ resources/js/functionalities/cubeAdditions.js
@@ -1,6 +1,11 @@
 import * as THREE from "three";
+import { randomColor } from "../utils/colors";
+import { randomPosition } from "../utils/position";
 
-export function addCube() {
+export function addCube({floorSize}) {
     // 
-    
+    let randomColor = randomColor();
+    let randomPosition = randomPosition(floorSize);
+    let randomSize = randomPosition();
+    let geo = new THREE.BoxGeometry()
 }
---
+++ resources/js/utils/size.js
@@ -0,0 +1,9 @@
+import { randFloatSpread } from "three/src/math/MathUtils.js";
+
+
+
+export function randomSize(maxSize = 5) {
+    // 
+    let size = Math.abs(randFloatSpread(maxSize));
+    return size;
+}

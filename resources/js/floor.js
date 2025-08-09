f74f752c56136464a663036554fc4bf9c013b4d5
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-08-09 21:14:21 +0100

    Adding utility function for creating floors

---
+++ resources/js/floor.js
@@ -0,0 +1,21 @@
+import * as THREE from "three";
+
+export function createFloor({size, color}) {
+    // 
+    let height, width;
+    if(size?? false) {
+        width = size.width; height = size.height
+    } else {
+        width = 1000; height = 1000;
+    }
+    // creating infinite floor
+    const geo = new THREE.PlaneGeometry(width, height);
+    const material = new THREE.MeshStandardMaterial();
+    material.roughness = 0.5;
+    material.metalness = 0.1;
+    material.color.set(color?? 0xffffff) ;
+
+    const floor = new THREE.Mesh(geo, material);
+    floor.name = "floor";
+    return floor;
+}

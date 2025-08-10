2f7ef42c9186e635aa99de34fb0418a5376cb8da
Hicham El Mouloudi <hichamelmouloudi04@gmail.com>
2025-08-10 04:44:44 +0100

    Adding Limit For zoom

--- resources/js/controls/orbitalControls.js
+++ resources/js/controls/orbitalControls.js
@@ -6,7 +6,7 @@ export function initializeOrbitsControls(camera, renderer) {
     orbitsControls.enableDamping = true;
     orbitsControls.dampingFactor = 0.05;
     orbitsControls.minDistance = 1;
-    orbitsControls.maxDistance = 100;
+    orbitsControls.maxDistance = 50;
     orbitsControls.minPolarAngle = Math.PI / 4;
     orbitsControls.maxPolarAngle = (3 * Math.PI) / 4;
 
